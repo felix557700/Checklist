b96051dab6dcfc14b2f233c76c728b68acbdaba9
Filip Vitas <felix557700@hotmail.com>
2016-04-06 00:23:41 +0200

    change express compression order to compress static assets

--- src/server.js
+++ src/server.js
@@ -11,11 +11,11 @@ import MongoDb from './db'
 const app = express();
 let secret = process.env.SECRET || 'here is my secret';
 
+app.use(compression());
 app.use(express.static(__dirname + '/../frontend_public'));
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(compression());
+app.use(logger('dev'));
 app.disable("x-powered-by");
 
 app.use(expressJwt({secret: secret}).unless({path: ['/api/users/login', '/api/users/register', '*.html', '*.js', '*.css', '*.ico']}));
