afc32bff543d93608bec55c52ce18e385545f5dc
Filip Vitas <felix557700@hotmail.com>
2016-04-19 01:27:13 +0200

    add config for hostname, port and mongolab

--- src/db.js
+++ src/db.js
@@ -5,11 +5,12 @@ let database;
 export default class MongoDb {
 	constructor() {
 		this.MongoClient = MongoClient;
+		this.DB_URL = process.env.MONGOLAB_URL || 'mongodb://localhost:27017/checklist';
 	}
 
 	connectToMongo() {
 		return new Promise((resolve, reject) => {
-			this.MongoClient.connect('mongodb://localhost:27017/checklist', function (err, db) {
+			this.MongoClient.connect(this.DB_URL, function (err, db) {
 				if (!err) {
 					console.log('We are connected to mongodb');
 					database = db;
--- src/server.listen.js
+++ src/server.listen.js
@@ -3,6 +3,6 @@ import http from 'http';
 export default function startServer(app) {
 	let server = http.createServer(app);
 	let port = process.env.PORT || 3000;
-	let hostname = 'localhost';
+	let hostname = process.env.HOST_NAME || 'localhost';
 	server.listen(port, hostname, () => console.log(`Magic is happening on ${hostname}:${port}`));
 }
